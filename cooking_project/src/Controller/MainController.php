ddc5b811a6550e474a0917e31ca6d7be940ecfb9
Antony <antony.jacques@laposte.net>
2020-11-05 14:18:59 +0100

    3 liste de toutes les recettes

--- cooking_project/src/Controller/MainController.php
+++ cooking_project/src/Controller/MainController.php
@@ -6,20 +6,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\DataFixtures\AppFixtures;
+use App\Entity\Meal;
+use App\Repository\MealRepository;
 
 class MainController extends AbstractController
 {
     /**
      * @Route("/main", name="main")
      */
-    public function index(): Response
+    public function index(MealRepository $repository): Response
     {
-        $data = file_get_contents('https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772');
-        $json_data = json_decode($data, true);
-        dd($json_data['meals'][0]);
+        $meallist = $repository->findAll();
 
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
+            'meallist'=>$meallist
         ]);
     }
+
+    
 }
